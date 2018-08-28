efe25d62b9a872024197d31a51eaf0db726e0b3f
tianmingzhang <tianming_2000@hotmail.com>
2018-08-28 20:46:24 +0800

    2018/8/26

--- source/Todo/Tori_detail.py
+++ source/Todo/Tori_detail.py
@@ -41,11 +41,13 @@ def detail_display():
         buttonflag = "false"
         authchecked = "false"
         finishchecked = "false"
+        entries = []
     #变更/删除的场合
     elif item_id.strip() != '':
         buttonflag = "true"
         # 取出item_id对应的TODO(除id以外全字段)
         undolist = mongo.todo.find({'_id': ObjectId(item_id)},{"_id": 0, "by":0})
+        entries = []
 
         for row in undolist:
             for key, value in row.items():
@@ -88,11 +90,11 @@ def detail_display():
                         finishchecked = "true"
 
                 #附件
-                entries = []
-                tempdict = {}
                 if key.startswith('附件'):
-                    tempdict['fileid']  = value['fileid']
-                    tempdict['filename'] = value['filename']
+                    tempdict = {}
+                    tempdict['fileid']  = value[0]['fileid']
+                    tempdict['filename'] = value[0]['filename']
+                    tempdict['fieldname'] = key
                     entries.append(tempdict)
 
     return render_template('index.html', item_id=item_id, buttonflag=buttonflag, authchecked=authchecked, finishchecked=finishchecked, sw3_name_hidden_value=sw3_name_hidden_value, sw4_name_hidden_value=sw4_name_hidden_value,entries=entries)
@@ -233,6 +235,23 @@ def content_update(check_error,internal_check_error):
                                         ISOTIMEFORMAT1)
 
         mongo.todo.update_one({'_id': item_id},{'$set':{'标题':title,'内容':content,'权限':auth,'预定完了日':scheduleday,'必须完了日':mustday,'创建日':creatday,'完了是否':finished,'创建者':'ztm'}},True)
+        #新规时附件追加
+        if item_id1.strip() == '':
+            file_list = getattr(g, 'file_list', None)
+            num = 1
+            for row in file_list:
+                for key, value in row.items():
+                    if key == 'fileid':
+                        file_id = value
+                    if key == 'filename':
+                        file_name = value
+                title = '附件' + num
+                title_value = {'fileid': file_id, 'filename': file_name}
+                mongo.todo.update_one({'_id': item_id}, {'$push': {title: title_value}}, False)
+                num = num + 1
+            if hasattr(g, "file_list"):
+                g.pop('file_list', None)
+
         from_index_return_list_value = "true"
         return render_template('list.html', from_index_return_list_value=from_index_return_list_value)
 
@@ -249,8 +268,27 @@ def add_file():
     resulta = {}
 
     if item_id1.strip() == '':
-        #新规场合：_id先暂存，依旧可以删除（从_id list)，查看，但是正式登陆要随新规按钮一起动作
-        pass
+        #新规场合：把文件放入 GridFS里，同时存住file_id，依旧可以删除（从_id list)，下载，但是正式登陆要随新规按钮一起动作
+        mongo1 = getattr(g, 'db', None)
+        mongo = mongo1.Tori
+        fs = gridfs.GridFS(mongo)
+        with fs.new_file(filename=file.filename) as fp:
+          fp.write(file)
+        # 返回处理结果
+        resulta['result'] = 0
+        resulta['mode'] = 0  # 新规模式
+        resulta['filename'] = file.filename
+        resulta['fileid'] = str(fp._id)
+
+        #存住file_id
+        file_list = []
+        tempdict = {}
+        tempdict['fileid'] = str(fp._id)
+        tempdict['filename'] = file.filename
+        file_list.append(tempdict)
+        if 'file_list' not in g:
+            g.file_list = file_list
+
     else:
         # 更新：_id直接存入对应的document，删除也直接从document，文件和更新按钮动作分开
         item_id = ObjectId(item_id1)
@@ -320,7 +358,8 @@ def del_file(fileid):
 
     #getparameter
     item_id1 = request.args.get('item_id', '', type=str)
-    item_id = ObjectId(item_id1)
+    if item_id1.strip() != '':
+        item_id = ObjectId(item_id1)
     fieldname = request.args.get('fieldname', '', type=str)
 
     with fs.get(ObjectId(fileid)) as fp_read:
@@ -329,28 +368,42 @@ def del_file(fileid):
     #中间文件删除
     try:
       os.remove(dir+filename)
-    except OSError as e:
-        resulta['result'] = 1
-        return resulta
+    #except OSError as e:
+        #resulta['result'] = 1
+        #return jsonify(result=resulta)
     except Exception as e:
         print(e)
 
     #GridFS里的文件删除
     try:
-      if fs.exists(fileid) :
-         fs.delete(fileid)
+      if fs.exists({"_id": ObjectId(fileid)}) :
+         fs.delete(ObjectId(fileid))
     except Exception as e:
         resulta['result'] = 1
-        return resulta
+        return jsonify(result=resulta)
 
-    # record记录里的file-id删除
-    try:
-        mongo.todo.update_one({'_id': item_id}, {'$unset': {fieldname: 1}})
-    except Exception as e:
-        resulta['result'] = 1
-        return resulta
+
+
+    if item_id1.strip() == '':
+      #新规的场合，file_id list里对应记录删除
+      file_list = getattr(g, 'file_list', None)
+      for row in file_list:
+          for key, value in row.items():
+              if key == 'fileid':
+                  file_id = value
+              if file_id == fileid:
+                  file_list.remove(row)
+                  break
+
+    else:
+      # 如果是更新的话，record记录里的file-id删除
+      try:
+          mongo.todo.update_one({'_id': item_id}, {'$unset': {fieldname: 1}})
+      except Exception as e:
+          resulta['result'] = 1
+          return jsonify(result=resulta)
 
     # 返回处理结果
     resulta['result'] = 0
-    resulta['mode'] = 1  # 更新模式
-    return resulta
+    #resulta['mode'] = 1  # 更新模式
+    return jsonify(result=resulta)
